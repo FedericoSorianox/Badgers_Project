41e26b6d6ca4f68f8bb46ed6f5fca6d88d3547ac
FedericoSorianox <129074525+FedericoSorianox@users.noreply.github.com>
2025-06-09 10:10:46 -0300

    Create urls.py

---
+++ backend/badgers_project/urls.py
@@ -0,0 +1,14 @@
+# badgers_project/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')), # Conecta nuestra API
+]
+
+# Para servir archivos de medios en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
